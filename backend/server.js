07b5beb94a37d573bec9e94ea369e699c831f001
sehamsayed248@gmail.com <sehamsayed248@gmail.com>
2025-05-09 14:59:21 +0300

    Auth Routes

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
-
+const cors = require('cors');
 const app = express();
+const authRoutes = require('./routes/authRoutes');
 
 app.use(express.json());
 
@@ -11,19 +12,23 @@ app.use(
     cors({
     origin: process.env.CLIENT_URL || "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "uthorization"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     })
 );
 
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true    useNewUrlParser & useUnifiedTopology have no effect since Node.js Driver version 4.0.0
 }).then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
-  app.get("/", (req, res) => res.send("API running"));
+app.get("/", (req, res) => res.send("API running"));
+
+
+app.use("/api/auth", authRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
-connectDB();
+// connectDB();
